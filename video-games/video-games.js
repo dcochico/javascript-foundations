3bdf3a9eb44cfaad284bafff497531b315dec396
Daniel Cochico <dcochico.20@gmail.com>
2023-03-28 12:26:02 -0700

    Complete video game exercises

--- video-games/video-games.js
+++ video-games/video-games.js
@@ -1,4 +1,41 @@
+function createPlayer(name,age,moveset) {
+    var player = {
+        name: name,
+        age: age,
+        moveset: moveset
+    }
+    return player;
+}
 
+function createLevel(name,players) {
+    var level = {
+        name: name,
+        players: players,
+        coins: 0,
+        lives: 3
+    }
+    return level;
+}
 
+function findCoins(level,coins) {
+    level.coins += coins;
+    if (level.coins >= 100) {
+        level.lives++;
+    }
+    return level;
+}
 
-module.exports = { };
+function defeatPlayer(level) {
+    level.lives--;
+    if (level.lives <= 0) {
+        return  'GAME OVER';
+    }
+    return level;
+}
+
+module.exports = {
+    createPlayer,
+    createLevel,
+    findCoins,
+    defeatPlayer,
+};
