9e10d400fe5e5c77db681bfa46a52505150d254f
Daniel Cochico <dcochico.20@gmail.com>
2023-03-27 20:27:46 -0700

    Complete favorite food exercises

--- favorite-foods/favorite-foods.js
+++ favorite-foods/favorite-foods.js
@@ -1,4 +1,43 @@
+function createFavoriteFood({dish,ingredients,isSpicy}) {
+    var food = {
+        name: dish,
+        ingredients: ingredients,
+        isSpicy: isSpicy,
+        timesOrdered: 0
+    }
+    return food;
+} 
 
+function commentOnSpiciness(food) {
+    if (food.isSpicy) {
+        return `Wow, this ${food.name} is so spicy!`;
+    }
+    return `Phew, this ${food.name} is not very spicy.`;
+}
 
+function orderFood(food) {
+    food.timesOrdered ++;
+    return food;
+}
 
-module.exports = { };
+function createShoppingList(foodList) {
+    var emptyList = [];
+    for (var i = 0; i < foodList.length; i++) {
+        emptyList.push(foodList[i].ingredients);
+    }
+    var filteredList = [];
+    for (var i = 0; i < emptyList.length; i++) {
+        var ingredients = emptyList[i].join('+');
+        filteredList.push(ingredients);
+    }
+    var finalList = filteredList.join('+');
+    finalList.replaceAll('++','+');
+    return finalList.split('+');
+}
+
+module.exports = {
+    createFavoriteFood,
+    commentOnSpiciness,
+    orderFood,
+    createShoppingList
+};
