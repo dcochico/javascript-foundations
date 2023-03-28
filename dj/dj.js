00bd57ce16d47c3d98bc5275c438f9b98690863b
Daniel Cochico <dcochico.20@gmail.com>
2023-03-27 18:55:54 -0700

    Complete dj exercises

--- dj/dj.js
+++ dj/dj.js
@@ -1,4 +1,41 @@
+function createSong(name, artist) {
+    var song = {
+        name: name,
+        artist: artist,
+        hasBeenPlayed: false
+    }
+    return song;
+}
 
+function playSong(song) {
+    song.hasBeenPlayed = true;
+    return song;
+}
 
+function makePlaylist(name, songs) {
+    var playlist = {
+        name: name,
+        songs: songs
+    }
+    return playlist;
+}
 
-module.exports = { };
+function addSongToPlaylist(playlist, song) {
+    playlist.songs.push(song);
+    return playlist;
+}
+
+function playAllSongs(playlist) {
+    for (var i = 0; i < playlist.songs.length; i++) {
+        playlist.songs[i].hasBeenPlayed = true;
+    }
+    return playlist;
+}
+
+module.exports = {
+    createSong,
+    playSong,
+    makePlaylist,
+    addSongToPlaylist,
+    playAllSongs
+};
