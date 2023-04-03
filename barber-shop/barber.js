c0a47c2b3e4514ff4f12509c8bfcc2c11164c75b
Daniel Cochico <dcochico.20@gmail.com>
2023-04-02 17:03:23 -0700

    Complete barber shop exercises

--- barber-shop/barber.js
+++ barber-shop/barber.js
@@ -1,3 +1,40 @@
+function createBarber(name, earnings, haircuts) {
+    var barber = {
+        name: name,
+        earnings: earnings,
+        haircuts: haircuts
+    }
+    if (earnings === undefined) {
+        barber.earnings = 0;
+    }
+    if (haircuts === undefined) {
+        barber.haircuts = [];
+    }
+    return barber;
+}
 
+function giveCompliment(haircut) {
+    return `This ${haircut.style} looks great!`;
+}
 
-module.exports = {  };
+function cutHair(barber, style) {
+    barber.haircuts.push(style);
+    barber.earnings += style.price;
+    return barber;
+}
+
+function listStyles(barber, hairLength) {
+    var lengthStyles = [];
+    for (var i = 0; i < barber.haircuts.length; i++) {
+        if (barber.haircuts[i].hairLength === hairLength) {
+            lengthStyles.push(barber.haircuts[i].style);
+        }
+    }
+    return lengthStyles;
+}
+module.exports = {
+    createBarber,
+    giveCompliment,
+    cutHair,
+    listStyles
+};
